258a5a0de7d88e1cf6b6b79c2c304774817b556f
teseo <sol@javiermellado.com>
2016-07-01 19:01:37 +0100

    bowling done

--- spec/BowlingGameSpec.php
+++ spec/BowlingGameSpec.php
@@ -5,6 +5,10 @@ namespace spec;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * Class BowlingGameSpec
+ * @package spec
+ */
 class BowlingGameSpec extends ObjectBehavior
 {
     function it_is_initializable()
@@ -16,19 +20,73 @@ class BowlingGameSpec extends ObjectBehavior
     {
         for ($i = 1; $i<=20; $i++)
         {
-           $this->roll(0);
+           $this->performBallLost();
         }
         $this->getResult()->shouldReturn(0);
     }
+
     function it_returns_6_points_by_hitting_pins_in_1_frame()
     {
        $this->roll(2);
        $this->roll(4);
+
         for ($i = 1; $i<=18; $i++)
         {
-           $this->roll(0);
+            $this->performBallLost();
         }
+        
         $this->getResult()->shouldReturn(6);
     }
 
+    function it_returns_20_points_by_having_a_spare_and_hitting_6_pins_in_the_next_frame()
+    {
+
+        $this->performSpare(7,3);
+
+        $this->roll(4);
+        $this->roll(2);
+
+        for ($i = 1; $i<=16; $i++)
+        {
+            $this->performBallLost();
+        }
+
+        $this->getResult()->shouldReturn(20);
+    }
+    function it_returns_28_points_by_having_a_strike_and_hitting_9_pins_in_the_next_frame()
+    {
+        $this->performStrike();
+
+        $this->roll(4);
+        $this->roll(5);
+
+        for ($i = 0; $i<17; $i++)
+        {
+            $this->performBallLost();
+        }
+
+        $this->getResult()->shouldReturn(28);
+    }
+    function it_returns_300_points_by_having_a_perfect_game()
+    {
+        for ($i = 0; $i< 12; $i++)
+        {
+           $this->performStrike();
+        }
+
+        $this->getResult()->shouldReturn(300);
+    }
+    private function performStrike()
+    {
+        $this->roll(10);
+    }
+    private function performSpare($ball1, $ball2)
+    {
+        $this->roll($ball1);
+        $this->roll($ball2);
+    }
+    private function performBallLost()
+    {
+        $this->roll(0);
+    }
 }
--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -1,16 +1,72 @@
 <?php
 
+/**
+ * Class BowlingGame
+ */
 class BowlingGame
 {
+    /**
+     * @var array
+     */
+    protected $plays = [];
+    /**
+     * @var int
+     */
     protected $result = 0;
 
+    const IS_SPARE = 10;
+    const IS_STRIKE= 10;
+    const MAX_ALLOWED_PLAYS = 20;
+    const MAX_ALLOWED_FRAMES_WITH_NO_BONUS = 10;
+
+    /**
+     * @param $hits
+     */
     public function roll($hits)
     {
-        $this->result += $hits;
+        $this->plays[] = $hits;
     }
 
+    /**
+     * @return int
+     */
     public function getResult()
     {
+        for ($i = 0; $i < self::MAX_ALLOWED_FRAMES_WITH_NO_BONUS; $i+=2  )
+        {
+            if ($this->isStrike($i))
+            {
+                $this->result+= $this->plays[$i]  + $this->plays[$i + 1] + $this->plays[$i + 2];
+                $i--;
+            }
+            else
+            {
+                $this->result+= $this->plays[$i] + $this->plays[$i + 1];
+
+                if($this->isSpare($i)){
+                    $this->result += $this->plays[$i + 2];
+                }
+            }
+        }
+
         return $this->result;
+    }
+
+    /**
+     * @param $play
+     * @return bool
+     */
+    private function isSpare($play)
+    {
+        return $this->plays[$play] + $this->plays[$play + 1] == self::IS_SPARE;
+    }
+
+    /**
+     * @param $play
+     * @return bool
+     */
+    private function isStrike($play)
+    {
+        return $this->plays[$play]  == self::IS_STRIKE;
     }
 }
